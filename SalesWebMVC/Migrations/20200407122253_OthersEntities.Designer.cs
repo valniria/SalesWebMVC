b66d9490467e33a0a26a8ce38da45c02c1f6af0c
Valniria Bandeira <xvalniria@gmail.com>
2020-04-07 09:24:49 -0300

    Other entities and second migration

--- SalesWebMVC/Migrations/20200407122253_OthersEntities.Designer.cs
+++ SalesWebMVC/Migrations/20200407122253_OthersEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebMVC.Data;
 
 namespace SalesWebMVC.Migrations
 {
     [DbContext(typeof(SalesWebMVCContext))]
-    partial class SalesWebMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20200407122253_OthersEntities")]
+    partial class OthersEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
